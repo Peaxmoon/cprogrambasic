556740278f4a1d734f35303f026b89dab94d62c4
peaxmoon <sujjalboi09@gmail.com>
2023-08-03 13:35:34 +0545

    the change in ifelse file

--- ifelse.c
+++ ifelse.c
@@ -4,13 +4,26 @@
 void main()
 {
     int a, b;
+    int i = 1;
     printf("Enter a number : ");
     scanf("%d",&a);
     printf("Enter b number : ");
     scanf("%d",&b);
-    if (a>b)
-    printf("a is greater.");
-    else
-    printf("b is greater.");
+    if (a>b){
+        printf("%d is greater than %d.\n", a, b);
+    }
+    else if (a==b){
+        printf("Both are equal.\n");
+    }
+    else{
+    printf("%d is greater than %d. \n", b, a);
+    }
+    while (i <= 5) {
+        printf("%d ", i);
+        i++; // Increment the value of i by 1 in each iteration
+    }
+    while(a<=57){
+        printf("%d \n",a);
+    }
     getch(); //if getch is used then the program will take instruction to close.
 }
