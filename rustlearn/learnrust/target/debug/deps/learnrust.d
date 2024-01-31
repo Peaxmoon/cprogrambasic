2af1c0e9711f446d2d5c56f1a20be7985a761034
Peaxmoon <sujjalboi09@gmail.com>
2024-01-31 22:06:51 +0545

    The format for rust \ cargo added

---
+++ rustlearn/learnrust/target/debug/deps/learnrust.d
@@ -0,0 +1,5 @@
+E:\c\cprogrambasic\rustlearn\learnrust\target\debug\deps\learnrust.exe: src\main.rs
+
+E:\c\cprogrambasic\rustlearn\learnrust\target\debug\deps\learnrust.d: src\main.rs
+
+src\main.rs:
---
+++ rustlearn/learnrust/target/debug/learnrust.d
@@ -0,0 +1,1 @@
+E:\c\cprogrambasic\rustlearn\learnrust\target\debug\learnrust.exe: E:\c\cprogrambasic\rustlearn\learnrust\src\main.rs
