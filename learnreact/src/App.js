20336febd4a1d6f0bcaf7ab8c17424c742b65f09
Peaxmoon <sujjalboi09@gmail.com>
2024-02-05 12:36:02 +0545

    The react main page discovered

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with sujjal khadka
         </a>
       </header>
     </div>
